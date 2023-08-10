5beebc82bf29d28f6bbe1804732efae7c49b799c
kissetfall <j@ergohaven.xyz>
2023-08-10 11:32:38 +0300

    keymap upd

--- keyboards/ergohaven/k02/keymaps/kissetfall/keymap.c
+++ keyboards/ergohaven/k02/keymaps/kissetfall/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,  KC_U,    KC_I,    KC_O,   KC_P,    KC_GRV,
          KC_LALT, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,  KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT,
          KC_ESC,  KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,  KC_M,    KC_COMM, KC_DOT, KC_QUOT, KC_MPLY,
-                   LALT(KC_LSFT), KC_LSFT, KC_LCTL, LOWER, KC_SPC,              KC_BSPC, RAISE, KC_RSFT, KC_RALT,  KC_RGUI 
+                         KC_VOLD, KC_VOLU, KC_LCTL, LOWER, KC_SPC,              KC_BSPC, RAISE, KC_RSFT, LALT(KC_LSFT),  KC_RGUI 
                                                
       ),
     
@@ -18,15 +18,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
          _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______,  _______,
          _______, KC_3,    KC_2,    KC_1,    KC_0,    KC_4,                              KC_7,    KC_6,    KC_5,    KC_9,    KC_8,     _______,
-         KC_ENT,  APP_A,   LALT(KC_TAB), APP_D, APP_F, APP_G,                       _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
-         _______, KC_LALT, APP_X,   APP_C,   APP_V,   APP_B,                             _______, PREVWRD, KC_PGDN, KC_PGUP, NEXTWRD,  _______,
-                           KC_VOLD, KC_VOLU, _______, _______, _______,         _______, ADJUST, _______, _______, _______ 
+         KC_ENT,  _______, APP_S,   KC_DEL,  ALT_TAB, APP_G,                             _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
+         _______, _______, _______, APP_C,   APP_V,   APP_B,                             _______, PREVWRD, KC_PGDN, KC_PGUP, NEXTWRD,  _______,
+                           KC_MPRV, KC_MNXT, _______, _______, _______,         _______, ADJUST, _______, _______, _______ 
     
       ),
     
      [_RAISE] = LAYOUT(
          _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-         _______, _______, KC_AT,   KC_EQL,  KC_PLUS, KC_LPRN,                           KC_RPRN, _______, KC_UNDS, KC_PERC, _______, _______,
+         _______, _______, KC_AT,   KC_EQL,  KC_PLUS, KC_LPRN,                           KC_RPRN, KC_TILD, KC_UNDS, KC_PERC, _______, _______,
          _______, KC_BSLS, KC_DLR,  KC_MINS, KC_HASH, KC_LBRC,                           KC_RBRC, KC_QUES, KC_SCLN, KC_CIRC, KC_EXLM, _______,
          _______, _______, KC_ASTR, KC_COLN, KC_SLSH, KC_LCBR,                           KC_RCBR, KC_PIPE, KC_TILD, KC_AMPR, _______, _______,
                            _______, _______, _______, ADJUST,  _______,         _______, _______, _______,  _______, _______ 
