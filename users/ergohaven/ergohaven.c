67a4590e4bc9703a68d8a2211ebb9457da7e51f5
Danil Tolkachev <tolkachev@nkbvs.ttn.ru>
2024-02-09 09:08:35 +0300

    support caps word for russian symbols

--- users/ergohaven/ergohaven.c
+++ users/ergohaven/ergohaven.c
@@ -135,7 +135,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
    return process_record_keymap(keycode, record);
 }
 
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes for russian symbols
+        case KC_SCLN:
+        case KC_QUOT:
+        case KC_LBRC:
+        case KC_RBRC:
+        case KC_GRAVE:
+            if (get_cur_lang() == LANG_RU) {
+                add_weak_mods(MOD_BIT(KC_LSFT));
+                return true;
+            } else
+                return false;
 
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT)); // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false; // Deactivate Caps Word.
+    }
+}
 
 void matrix_scan_user(void) { // The very important timer.
   if (is_alt_tab_active) {
