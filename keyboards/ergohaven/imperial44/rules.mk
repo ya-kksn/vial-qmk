d7c7e110b5b22117869931c9b72be9105ed64d3a
Danil Tolkachev <dandur.oak@gmail.com>
2024-05-05 21:30:55 +0300

    fix ci build-vial stage

--- keyboards/ergohaven/imperial44/rules.mk
+++ keyboards/ergohaven/imperial44/rules.mk
@@ -10,3 +10,21 @@ OLED_DRIVER = ssd1306
 SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = vendor
 RGBLIGHT_ENABLE = yes
+
+NKRO_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+LTO_ENABLE = no
+VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+
+SRC += keyboards/ergohaven/ergohaven_rgb.c
+SRC += keyboards/ergohaven/ergohaven.c
+SRC += keyboards/ergohaven/lang_ru_en.c
