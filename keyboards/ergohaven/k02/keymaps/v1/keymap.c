4aa4bcfdbddb03564755d1be5208a335a4845087
Danil Tolkachev <dandur.oak@gmail.com>
2024-05-06 09:04:28 +0300

    common oled code for k2

--- keyboards/ergohaven/k02/keymaps/v1/keymap.c
+++ keyboards/ergohaven/k02/keymaps/v1/keymap.c
@@ -1,11 +1,5 @@
 #include QMK_KEYBOARD_H
 #include "ergohaven.h"
-// #include "oled/bongocat.c"
-// #include "oled/ergohaven_light.c"
-#include "oled/ergohaven_dark.c"
-// #include "font_block.h"
-// #include "game/game.h"
-// #include "layers.c"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_BASE] = LAYOUT( \
@@ -31,126 +25,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          _______, _______, KC_ASTR, KC_COLN, KC_SLSH, _______,                          _______, KC_PIPE, KC_TILD, KC_AMPR, _______,  QK_RBT,
                            _______, _______, _______, ADJUST, _______,         _______, _______, _______, _______, _______ \
         ),
-    
+
       [_ADJUST] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, DM_PLY1, DM_REC1,
         RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,                           _______, KC_VOLD, KC_MUTE, KC_VOLU, DM_PLY2, DM_REC2,
         RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, CG_TOGG,                           _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, DM_RSTP,
-                          _______, _______, _______, _______, _______,         _______, _______,  _______, _______, _______                  
-                                                                          
+                          _______, _______, _______, _______, _______,         _______, _______,  _______, _______, _______
+
        ),
 };
-
-#ifdef OLED_ENABLE
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // bongocat, ergohaven_dark/light
-    // return OLED_ROTATION_270;  // luna 
-     }
-    else {
-    return OLED_ROTATION_270;  
-    }
-  return rotation;
-}
-
-void render_layer_state(void) {
-    // Print current mode
-    oled_write_ln_P(PSTR("K:02\n"), false);
-    /* oled_write_P(PSTR("\n"), false); */
-    oled_write_ln_P(PSTR("v3.3\n"), false);
-    oled_write_P(PSTR("\n"), false);
-    oled_write_ln_P(PSTR("MODE\n"), false);
-    if (keymap_config.swap_lctl_lgui) {
-        oled_write_ln_P(PSTR("Mac"), false);
-    } else {
-        oled_write_ln_P(PSTR("Win"), false);
-    }
-
-    oled_write_P(PSTR("\n\n"), false);
-    // Print current layer
-    oled_write_ln_P(PSTR("LAYER"), false);
-    switch (get_highest_layer(layer_state)) {
-        case _BASE:
-            oled_write_P(PSTR("Base\n"), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("Raise"), false);
-            break;
-        case _LOWER:
-            oled_write_P(PSTR("Lower"), false);
-            break;
-        case _ADJUST:
-            oled_write_P(PSTR("Adjst"), false);
-            break;
-        case _FOUR:
-            oled_write_P(PSTR("Four\n"), false);
-            break;
-        case _FIVE:
-            oled_write_P(PSTR("Five\n"), false);
-            break;
-        case _SIX:
-            oled_write_P(PSTR("Six\n"), false);
-            break;
-        case _SEVEN:
-            oled_write_P(PSTR("Seven"), false);
-            break;
-        case _EIGHT:
-            oled_write_P(PSTR("Eight"), false);
-            break;
-        case _NINE:
-            oled_write_P(PSTR("Nine\n"), false);
-            break;
-         case _TEN:
-            oled_write_P(PSTR("Ten\n"), false);
-            break;
-         case _ELEVEN:
-            oled_write_P(PSTR("Elevn"), false);
-            break;
-         case _TWELVE:
-            oled_write_P(PSTR("Twlve"), false);
-            break;
-         case _THIRTEEN:
-            oled_write_P(PSTR("Thrtn"), false);
-            break;
-         case _FOURTEEN:
-            oled_write_P(PSTR("Frtn\n"), false);
-            break;
-         case _FIFTEEN:
-            oled_write_P(PSTR("Fiftn"), false);
-            break;
-        default:
-            oled_write_ln_P(PSTR("Undef"), false);
-    }
-    oled_write_P(PSTR("\n"), false);
-    led_t led_usb_state = host_keyboard_led_state();
-    oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
-}
-
-
-// Used to draw on to the oled screen
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        render_layer_state();
-         // if (!isGamingMode()) {
-         //     render_layer_state();
-         // }
-    } else {
-    // render_bongocat();  // bongocat
-    // render_luna_status();  // luna
-        // ergohaven_light_draw();
-        ergohaven_dark_draw();
-}
-    return false;
-}
-
-// void matrix_scan_keymap(void) {
-//     if (isGamingMode()) {
-//         if (countMainTimer() > 0) {
-//             game_main();
-//         }
-//     }
-// }
-
-#endif
